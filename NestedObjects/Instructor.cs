db241b2a0fd1b4c698f12062c77fcf19d4021276
ATimko <ashleytimko777@gmail.com>
2019-02-27 13:48:42 -0800

    Added documentation to the instructor class

--- NestedObjects/Instructor.cs
+++ NestedObjects/Instructor.cs
@@ -8,10 +8,21 @@ namespace NestedObjects
 {
     class Instructor
     {
+        /// <summary>
+        /// The instuctors First and Last name
+        /// ex. Joseph Ortiz
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// The instuctors work (@cptc.edu) email
+        /// </summary>
         public string Email { get; set; }
 
+        /// <summary>
+        /// The office phone number for the
+        /// instructor
+        /// </summary>
         public string PhoneNumber { get; set; }
 
     }
